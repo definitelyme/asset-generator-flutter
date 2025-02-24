549ab55b516b3f49cfae0434ae8de109f4feb26b
Brendan <ejike.br@gmail.com>
2025-02-24 09:15:56 +0100

    build: bump deps
    - resolve breaking changes

--- src/utils/vs_code_actions.ts
+++ src/utils/vs_code_actions.ts
@@ -1,5 +1,5 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+var __awaiter = (globalThis && globalThis.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -60,4 +60,3 @@ class VsCodeActions {
     }
 }
 exports.VsCodeActions = VsCodeActions;
-//# sourceMappingURL=vs_code_actions.js.map
